71c339a2a5e7b4af3fab85cccbf05c42c7d93f15
sensen52 <gytls52tpdls@naver.com>
2023-09-17 12:17:25 +0900

    field_dependency_complete

--- ex00/src/test/java/com/example/ex00/dependency/DependencyTests.java
+++ ex00/src/test/java/com/example/ex00/dependency/DependencyTests.java
@@ -10,8 +10,8 @@ import lombok.extern.log4j.Log4j;
 
 // 이 클래스 파일은 was랑 별도로 돌아야됨 서버가 아니라 junit 프로그램을 돌리는것
 
-@RunWith(SpringJUnit4ClassRunner.class) //어떤 프로그램을 돌릴거냐
-@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@RunWith(SpringJUnit4ClassRunner.class) //어떤 프로그램을 돌릴것인가
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml") //xml파일 경로를 적어줌으로 써  runwith가 돌아갈때 xml을 참조해서 돌아감
 @Log4j
 public class DependencyTests {
 	
@@ -20,7 +20,10 @@ public class DependencyTests {
 	
 	@Test
 	public void checkDependencyInjection() {
-		
+		log.info("-----------------");
+		log.info("coding :"+coding);
+		log.info("computer :"+coding.getComputer());
+		log.info("-----------------");
 	}
 	
 }
