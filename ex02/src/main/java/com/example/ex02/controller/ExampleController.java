04e79f6675b78079786f59d191c9007f15d5d1f5
sensen52 <gytls52tpdls@naver.com>
2023-09-22 14:08:54 +0900

    framework2

---
+++ ex02/src/main/java/com/example/ex02/controller/ExampleController.java
@@ -0,0 +1,12 @@
+package com.example.ex02.controller;
+
+import org.springframework.stereotype.Controller;
+
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+public class ExampleController {
+
+	
+}
