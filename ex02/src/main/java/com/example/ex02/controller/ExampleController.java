cfa7bf33aba1feda6a36f71f5a1fdbf40146ce75
sensen52 <gytls52tpdls@naver.com>
2023-09-26 21:26:02 +0900

    .

--- ex02/src/main/java/com/example/ex02/controller/ExampleController.java
+++ ex02/src/main/java/com/example/ex02/controller/ExampleController.java
@@ -1,6 +1,8 @@
 package com.example.ex02.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.extern.log4j.Log4j;
 
@@ -8,5 +10,10 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class ExampleController {
 
+	@GetMapping("checkIn")
+	public String chekIn(Model model) {
+		
+		return "/work/checkIn"
+	}
 	
 }
