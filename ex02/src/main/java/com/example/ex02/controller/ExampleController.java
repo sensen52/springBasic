427ee7d1b8ed8abdfe2da7c7710a3a81b3ca2b63
sensen52 <gytls52tpdls@naver.com>
2023-09-29 22:09:40 +0900

    Calendar work & leave system

--- ex02/src/main/java/com/example/ex02/controller/ExampleController.java
+++ ex02/src/main/java/com/example/ex02/controller/ExampleController.java
@@ -1,19 +1,53 @@
 package com.example.ex02.controller;
 
+import java.util.Calendar;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import lombok.extern.log4j.Log4j;
 
 @Controller
 @Log4j
 public class ExampleController {
+//이름을 입력, 출근 혹은 퇴근 버튼을 클릭한다.
+//출근시간은 9:00, 퇴근시간은 18:00
+//출근 9시 이후면 지각처리, 퇴근시간 전에 퇴근버튼 누르면 업무시간으로 처리
+//모든 jsp는 work 폴더안에 분기별로getToWork, leaveWork, late,work.jsp
 
-	@GetMapping("checkIn")
-	public String chekIn(Model model) {
-		
-		return "/work/checkIn"
+	@GetMapping("/checkIn")
+	public String chekIn() {
+
+		return "work/checkIn";
 	}
+
+	@GetMapping("/getToWork")
+	public String getToWork(@ModelAttribute("name") String name) {
+		Calendar c = Calendar.getInstance();
+		int hour = c.get(Calendar.HOUR_OF_DAY);
+		int minute =c.get(Calendar.MINUTE);
+	//	log.info("출근 들어옴");
+		
+		boolean lateCondition = hour >= 9 && minute >0;
+		if(lateCondition) {
+			return "work/late";
+		}return "work/getToWork";
+		
+	}
+
+	@GetMapping("/leaveWork")
+	public String leaveWork(@ModelAttribute("name") String name) {
+	//log.info("퇴근 들어옴");
+		
+		Calendar c = Calendar.getInstance();
+		int hour = c.get(Calendar.HOUR_OF_DAY);
+		int minute =c.get(Calendar.MINUTE);
 	
+		
+		boolean leaveWorkCondition = hour >= 18 && minute >=0;
+		if(leaveWorkCondition) {
+			return "work/leaveWork";
+		}return "work/work";
+	}
 }
--- ex02/src/main/webapp/WEB-INF/views/work/checkIn.jsp
+++ ex02/src/main/webapp/WEB-INF/views/work/checkIn.jsp
@@ -1,13 +1,30 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+        <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>체크인</title>
 </head>
 <body>
+<form action="/getToWork" name="checkInForm">
+ <input type="text" name="name" />
+ <br>
+ <button>출근</button>
+ <button type="button">퇴근</button>
+</form>
 
+<script>
+let form = checkInForm;
+let button = document.querySelector("button[type='button']");
+form.setAttribute("action","/getToWork");
+//console.log(form.getAttribute("action"));
+button.addEventListener("click",function(){
+	form.setAttribute("action","/leaveWork");
+	form.submit();
+})
 
+</script>
 </body>
 </html>
